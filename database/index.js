ee764b888922e8ef7d22b386c7ab930df2c555c2
Teddy An <teddy@Teddys-MacBook-Pro.local>
2018-12-20 16:22:56 -0800

    Create schemas and connect to database

---
+++ database/index.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'reviews'
+});
+
+connection.connect(function (err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log('Connected!');
+  }
+});
+
+module.exports = connection;
