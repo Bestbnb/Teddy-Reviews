2f6cc7eb591c66fa06b5e6375506a21a23cbaac6
Teddy An <teddy@Teddys-MacBook-Pro.local>
2018-12-21 17:12:11 -0800

    Can populate db with random data

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
 const faker = require('faker');
 
-const connection = new Sequelize('reviews', 'root', '', {
+const connection = new Sequelize('reviewsDB', 'root', '', {
   host: 'localhost',
   dialect: 'mysql'
 })
+
+// return connection.query("CREATE DATABASE IF NOT EXISTS `reviews`;")
 
 connection.authenticate()
   .then(() => {
@@ -25,7 +27,50 @@ const Users = connection.define('users', {
   profile_page: Sequelize.STRING
 })
 
+const Stars = connection.define('stars', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  accuracy: Sequelize.INTEGER,
+  communication: Sequelize.INTEGER,
+  cleanliness: Sequelize.INTEGER,
+  location: Sequelize.INTEGER,
+  check_in: Sequelize.INTEGER,
+  value: Sequelize.INTEGER
+})
+
+const Reviews = connection.define('reviews', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  date_published: Sequelize.DATE,
+  review: Sequelize.STRING
+})
+
+Users.belongsTo(Reviews);
+Users.belongsTo(Stars);
+Stars.belongsTo(Reviews);
+
 Users.sync();
+Stars.sync();
+Reviews.sync();
+
+Users.destroy({
+  where: {},
+  truncate: true
+});
+Stars.destroy({
+  where: {},
+  truncate: true
+});
+Reviews.destroy({
+  where: {},
+  truncate: true
+});
 
 for (let i = 0; i < 100; i++) {
   let dummyDataUsers = {
@@ -34,25 +79,26 @@ for (let i = 0; i < 100; i++) {
     profile_page: faker.internet.url()
   };
   Users.create(dummyDataUsers);
+  
+  let dummyDataStars = {
+    accuracy: faker.random.number(5),
+    communication: faker.random.number(5),
+    cleanliness: faker.random.number(5),
+    location: faker.random.number(5),
+    check_in: faker.random.number(5),
+    value: faker.random.number(5)
+  };
+  Stars.create(dummyDataStars);
+  
+  let dummyDataReviews = {
+    date_published: faker.date.past(3),
+    review: faker.lorem.sentence()
+  };
+  Reviews.create(dummyDataReviews);
 }
 
-// const add100Users = (req, res) => {
-//   for (var i = 0; i < 100; i++) {
-//     var randomName = faker.name.findName();
-//     var randomProfilePage = faker.internet.url();
-//     var randomProfilePic = faker.image.avatar();
-    
-//     const query = `INSERT INTO users (name, profile_picture, profile_page) VALUES (${randomName}, ${randomProfilePage}, ${randomProfilePic});`;
-//     db.query(query, (err, results) => {
-//       if(err) {
-//         res.status(501).send();
-//       } else {
-//         res.status(201).send();
-//       }
-//     })
-//   }
-// }
-
 module.exports = {
-  Users
+  Users,
+  Stars,
+  Reviews
 };
