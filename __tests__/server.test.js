bbb5b1317e03a76ea916b2b2a834f8450136cc7e
Teddy An <teddy@Teddys-MacBook-Pro.local>
2019-01-03 00:08:45 -0800

    Start test development

---
+++ __tests__/server.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('../server/index.js');
+
+describe('tests reviews path', () => {
+  test('the api request has json Content-Type', () => request(app)
+    .get('/api/home/1/reviews')
+    .expect('Content-Type', /json/));
+  test('should respond with reviews to a valid path', (done) => {
+    request(app).get('/api/home/1/reviews').then((response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+  test('should respond with statusCode 404 to invalid path', (done) => {
+    request(app).get('/api/home/1/rev').then((response) => {
+      expect(response.statusCode).toBe(404);
+      done();
+    });
+  });
+});
